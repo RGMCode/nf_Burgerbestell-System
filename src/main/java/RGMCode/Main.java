3785bcbc7ea516e48380b4ab79c706899bf9e9d3
mrg <rgmengedoth@gmail.com>
2023-07-13 16:37:07 +0200

    add some things

--- src/main/java/RGMCode/Main.java
+++ src/main/java/RGMCode/Main.java
@@ -6,6 +6,7 @@ public class Main {
         orderSystem.addMenu(new Menu(1, "Cheeseburger Menu", 7.99, "Cheeseburger", "Fries", "Cola"));
         orderSystem.addMenu(new Menu(2, "Chicken Burger Menu", 8.99, "Chicken Burger", "Fries", "Sprite"));
 
+        orderSystem.printMenus();
         orderSystem.placeOrder();
     }
 }
