3112a3d807bb671e61258468d33c9f978b21fe7b
mrg <rgmengedoth@gmail.com>
2023-07-13 16:23:29 +0200

    add some things

--- src/main/java/RGMCode/OrderSystem.java
+++ src/main/java/RGMCode/OrderSystem.java
@@ -1,5 +1,13 @@
 package RGMCode;
 
+/*      Aufgabe Burgerbestell System (Seite 2 von 2)
+        OrderSystem:
+        - Enthält eine Map von Menus.
+        - Erstelle eine addMenu Methode um deinen Gästen neue Menüs anbieten zu können.
+        - Schreibe eine getOrderById Methode, die dir das entsprechende Menü aus der Map zurückgibt.
+        - Ergänze eine placeOrder Methode, die eine Eingabe des Users per Konsole erwartet und getOrderById aufruft. Ergänze
+          diese mit der Ausgabe der bestellten Artikel im Menu und dem Preis.
+*/
 
 public class OrderSystem {
 }
