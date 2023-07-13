bfba03a3571921d7e7f9feef3a1d01dd946dbe1a
mrg <rgmengedoth@gmail.com>
2023-07-13 16:18:25 +0200

    Initial commit

---
+++ src/main/java/RGMCode/Menu.java
@@ -0,0 +1,24 @@
+package RGMCode;
+
+/*      Aufgabe Burgerbestell System(Seite 1von 2)
+        Erstelle ein kleines Programm mit dem der Benutzer ein Burgermenü bestellen kann,indem er
+        dessen Menünummer angibt.
+
+        Das bestellte Menü wird mit Inhalt und Preis ausgeben.
+        Es soll eine Menu und eine OrderSystem Klasse enthalten.
+
+        Menu:
+        - Menu enthält den name,price,mainDish,sideDish,beverage,überlege dir hierfür sinnvolle Datentypen.
+        - Überlege welche Methoden du noch benötigst und ergänze sie.
+*/
+public class Menu {
+
+    private String name;
+    private double price;
+    private String mainDish;
+    private String sideDish;
+    private String beverage;
+
+
+
+}
